50d6ff416334db973c51651b3e05785df339756e
José Pinto <jjmppinto@gmail.com>
2025-02-03 11:26:30 +0000

    project created

---
+++ Leilao_Produtos_Spring_Boot/src/main/java/com/example/leilao_produtos_spring_boot/LeilaoProdutosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.leilao_produtos_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeilaoProdutosSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeilaoProdutosSpringBootApplication.class, args);
+    }
+
+}
---
+++ Leilao_Produtos_Spring_Boot/src/test/java/com/example/leilao_produtos_spring_boot/LeilaoProdutosSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leilao_produtos_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeilaoProdutosSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
