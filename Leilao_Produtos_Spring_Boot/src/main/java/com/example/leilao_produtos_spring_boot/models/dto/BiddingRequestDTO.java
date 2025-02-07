7d60d19f7ddbd4d2e517b177db4b3ea72e287220
JoséGranja <zezeada06@gmail.com>
2025-02-07 14:16:23 +0000

    Update BiddingRequestDTO.java

--- Leilao_Produtos_Spring_Boot/src/main/java/com/example/leilao_produtos_spring_boot/models/dto/BiddingRequestDTO.java
+++ Leilao_Produtos_Spring_Boot/src/main/java/com/example/leilao_produtos_spring_boot/models/dto/BiddingRequestDTO.java
@@ -7,13 +7,19 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 
+/**
+ * DTO (Data Transfer Object) para a criação de uma nova licitação (lance).
+ *
+ * - Utilizado para receber os dados da requisição ao criar uma nova requesição.
+ * - Evita expor diretamente a entidade Bidding na API.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BiddingRequestDTO {
-    private int id;
-    private int auctionId;
-    private int userId;
-    private BigDecimal bidAmount;
+    private int id; // Identificador da licitação (não precisa ser enviado na requisição)
+    private int auctionId; // ID do leilão onde a licitação está a ser feita
+    private int userId; // ID do utilizador que está a fazer a licitação
+    private BigDecimal bidAmount; // Valor da licitação oferecida
 }
