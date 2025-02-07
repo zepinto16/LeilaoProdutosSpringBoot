2e6dd693695b855de5e3e763c8bb67254a47c06b
JoséGranja <zezeada06@gmail.com>
2025-02-07 14:36:02 +0000

    Update UserDTO.java

--- Leilao_Produtos_Spring_Boot/src/main/java/com/example/leilao_produtos_spring_boot/models/dto/UserDTO.java
+++ Leilao_Produtos_Spring_Boot/src/main/java/com/example/leilao_produtos_spring_boot/models/dto/UserDTO.java
@@ -8,6 +8,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
 
+/**
+ * DTO (Data Transfer Object) para a entidade User (Usuário).
+ *
+ * - Utilizado para transferir os dados do usuário entre a API e o sistema.
+ * - Evita expor diretamente a entidade User na API.
+ * - Suporta HATEOAS para permitir a adição de links na resposta da API.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
@@ -15,16 +22,22 @@ import org.springframework.hateoas.RepresentationModel;
 
 public class UserDTO extends RepresentationModel<UserDTO> {
 
-    private int id;
+    private int id; // Identificador único do usuário
 
-    String name;
+    String name; // Nome do utilizador
 
-    private int age;
+    private int age; // Idade do utilizador
 
-    private String email;
+    private String email; // E-mail do utilizador
 
-    private Gender gender;
+    private Gender gender; // Género do utilizador
 
+    /**
+     * Método estático para converter uma entidade User em um UserDTO.
+     *
+     * @param u Objeto User a ser convertido.
+     * @return Objeto UserDTO contendo os mesmos dados do utilizador original.
+     */
     public static UserDTO toUserDTO(User u){
         return new UserDTO(u.getId(), u.getName(), u.getAge(), u.getEmail(), u.getGender());
     }
