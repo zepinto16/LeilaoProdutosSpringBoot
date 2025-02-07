a52f2750ad3deccb920ef0e6a67c01696e2b5539
JoséGranja <zezeada06@gmail.com>
2025-02-07 13:50:27 +0000

    Update AuctionRequestDTO.java

--- Leilao_Produtos_Spring_Boot/src/main/java/com/example/leilao_produtos_spring_boot/models/dto/AuctionRequestDTO.java
+++ Leilao_Produtos_Spring_Boot/src/main/java/com/example/leilao_produtos_spring_boot/models/dto/AuctionRequestDTO.java
@@ -3,9 +3,15 @@ package com.example.leilao_produtos_spring_boot.models.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * DTO (Data Transfer Object) para criar um novo leilão (Auction).
+ *
+ * - Utilizado para receber os dados da requisição ao criar um novo leilão.
+ * - Evita expor diretamente a entidade Auction na API.
+ */
 @Getter
 @Setter
 public class AuctionRequestDTO {
-    private String name;
-    private String description;
+    private String name; // Nome do leilão
+    private String description; // Descrição do leilão
 }
