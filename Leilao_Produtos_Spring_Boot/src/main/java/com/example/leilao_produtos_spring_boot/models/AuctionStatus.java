eb56a4d78c6ff47193f9a61baefcc375cd83e530
JoséGranja <zezeada06@gmail.com>
2025-02-07 11:47:48 +0000

    Update AuctionStatus.java

--- Leilao_Produtos_Spring_Boot/src/main/java/com/example/leilao_produtos_spring_boot/models/AuctionStatus.java
+++ Leilao_Produtos_Spring_Boot/src/main/java/com/example/leilao_produtos_spring_boot/models/AuctionStatus.java
@@ -1,8 +1,11 @@
 package com.example.leilao_produtos_spring_boot.models;
 
+/**
+ * Enumeração que define os possíveis status de um leilão.
+ */
 public enum AuctionStatus {
 
-    ACTIVE,
-    CLOSED
+    ACTIVE, // Leilão está aberto para licitações
+    CLOSED // Leilão foi encerrado
 
 }
