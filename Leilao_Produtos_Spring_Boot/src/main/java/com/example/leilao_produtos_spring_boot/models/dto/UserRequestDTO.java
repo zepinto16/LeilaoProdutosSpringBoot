6398b0a1ab22547722c6198bfb24f18410b9cffc
JoséGranja <zezeada06@gmail.com>
2025-02-07 14:39:11 +0000

    Update UserRequestDTO.java

--- Leilao_Produtos_Spring_Boot/src/main/java/com/example/leilao_produtos_spring_boot/models/dto/UserRequestDTO.java
+++ Leilao_Produtos_Spring_Boot/src/main/java/com/example/leilao_produtos_spring_boot/models/dto/UserRequestDTO.java
@@ -3,15 +3,22 @@ package com.example.leilao_produtos_spring_boot.models.dto;
 import com.example.leilao_produtos_spring_boot.models.Gender;
 import lombok.*;
 
+/**
+ * DTO (Data Transfer Object) para receber os dados ao criar ou atualizar um utilizador.
+ *
+ * - Utilizado para capturar os dados da requisição ao criar ou atualizar um utilizador.
+ * - Evita expor diretamente a entidade User na API.
+ * - Melhora a segurança e a organização dos dados recebidos.
+ */
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class UserRequestDTO {
-    private String name;
-    private int age;
-    private String email;
-    private Gender gender;
+    private String name; // Nome do utilizador
+    private int age; // Idade do utilizador
+    private String email; // Endereço de e-mail do utilizador
+    private Gender gender; // Género do utilizador
 }
 
