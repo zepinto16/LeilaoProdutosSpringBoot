e08a6b1c565fa1edfbfa0e9fd6ac267eda5e843c
JoséGranja <zezeada06@gmail.com>
2025-02-07 14:29:58 +0000

    Update SaleRequestDTO.java

--- Leilao_Produtos_Spring_Boot/src/main/java/com/example/leilao_produtos_spring_boot/models/dto/SaleRequestDTO.java
+++ Leilao_Produtos_Spring_Boot/src/main/java/com/example/leilao_produtos_spring_boot/models/dto/SaleRequestDTO.java
@@ -2,11 +2,17 @@ package com.example.leilao_produtos_spring_boot.models.dto;
 
 import lombok.*;
 
+/**
+ * DTO (Data Transfer Object) para representar uma solicitação de venda de um item no sistema de leilão.
+ *
+ * - Utilizado para receber os dados da requisição ao registrar uma venda.
+ * - Evita expor diretamente a entidade Sale na API.
+ */
     @Getter
     @Setter
     @NoArgsConstructor
     @AllArgsConstructor
     public class SaleRequestDTO {
-        private int itemId;
+        private int itemId; // Identificador do item que está a ser vendido
     }
 
